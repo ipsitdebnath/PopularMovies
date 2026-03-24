ad002c0f4421f0352b576005e0029b02fb5336bf
Ipsit-Debnath <ipsitdebnath5@gmail.com>
2026-03-24 23:32:59 +0530

    added index.css and added info from api in the UI

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 async function getMovies() {
-    try {
-        let response = await fetch ("https://api.themoviedb.org/3/movie/popular?api_key=1829dffd986a3aa94677f730051f78f7");
+  try {
+    let response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=1829dffd986a3aa94677f730051f78f7",
+    );
     let data = await response.json();
     return data.results;
-    
-    } catch (error) {
-        console.log("Error in fetching API");
-    }
+  } catch (error) {
+    console.log("Error in fetching API");
+  }
 }
 
-async function main(){
-    let movies20 = await getMovies();
-    console.log(movies20);
-    let result = document.getElementById("result")
-    let resultContent = "";
-    movies20.forEach((movie,idx) => {
-        resultContent += `${idx+1}. ${movie.original_title} \n`;
-    });
-    result.innerText = resultContent;
+async function main() {
+  let movies20 = await getMovies();
+  console.log(movies20);
+  let result = document.getElementById("result");
+  let resultContent = "";
+  movies20.forEach((movie, idx) => {
+    resultContent += `${idx + 1}. ${movie.original_title} \n Original Language: ${movie.original_language} \n Overview: ${movie.overview} \n Popularity: ${movie.popularity} \n Average vote: ${movie.vote_average} \n Vote count: ${movie.vote_count} \n`;
+  });
+  result.innerText = resultContent;
 }
 main();
